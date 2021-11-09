21dd617fa8a88553eed24279457405d6e1090b12
Madison Call <tcall@zagmail.gonzaga.edu>
2021-11-08 17:02:10 -0800

    Added a really basic radio button question to quiz. Also put vscode into the .gitignore

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -3,13 +3,16 @@ import React from 'react';
 import './QuizPage.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import QuizMenubar from './QuizMenubar';
+import Questions from './Questions'
 
 const QuizPage = () => {
     return (
         <>
         <QuizMenubar></QuizMenubar>
         <Container className="quizPageContainer">
-
+            <Questions>
+                
+            </Questions>
         </Container>
         </>
     );
