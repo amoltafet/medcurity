992f249f96e14c47a1ad645019b46580f36fe42e
Madison Call <tcall@zagmail.gonzaga.edu>
2021-11-12 16:38:36 -0800

    Started working on dynamic question generation

--- src/Quiz/Questions.js
+++ src/Quiz/Questions.js
@@ -2,8 +2,31 @@ import React from 'react';
 import {ToggleButtonGroup, ToggleButton} from 'react-bootstrap';
 import './Questions.css';
 
-
+/**
+ * 
+ * @param {answers} props 
+ */
 function Questions(props) {
+    const buttonGroup = React.createElement(ToggleButtonGroup, {
+        vertical: true,
+        name: "Q1"
+    }, null)
+
+    for(let answer in props.answers) {
+        const button = React.createElement(ToggleButton, {
+            id: "tbj-check-1",
+            value: 1,
+            variant: "light"
+          }, "This is my button........................................");
+        // button.classList.add('btn')
+        // button.addEventListener('click', selectAnswer)
+        //answerButtonsElement.appendChild(button)
+        buttonGroup.appendChild(button)
+    };
+    return buttonGroup;
+}
+
+function __Questions(props) {
     const answers = props.answers;
     
     return (
