eee1e5c144fe80eb01cc0fea216e126d52456e40
evpching <Q4T7B1Yv0ltal!@>
2021-11-23 20:38:42 -0800

    add comments for clarification

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -10,27 +10,33 @@ import axios from 'axios';
 /**
  * 
  * @param {str} category Question category to get questions from
- * @param {int} index Index of question 
+ * @param {int} index Index of question in the returned query
  */
 function GetQuestionByIndex(category, index)
 {
-  const [question, getQuestions] = useState([]);
-
+  // The route for axios.get() to use to query the db
   const url = 'http://localhost:3002/api/getCategoryQuestions'
+
+  // I am unsure why useState() and useEffect() makes this work,
+  // but plenty of online examples I viewed used this pattern.
+  const [question, getQuestions] = useState([]);
 
   useEffect(() => {
     getQuestionsIncategory();
   }, [])
 
+  // Create a GET HTTP request that uses the getCategoryQuestions route to 
+  // query the db for questions in the specified category.
+  // Then choose a question from the query result using the index param.
   const getQuestionsIncategory = () => {
     axios.get(`${url}`, { params: { filter: category } }).then((response) => {
       const data = response.data
-      console.log(data.length)
-
       getQuestions(data[index]);
     }).catch(error => console.error(error));
   }
 
+  // Return the question component with question info from the query
+  // into the components props.
   return (
     <Questions 
     question={question.question}
--- src/_server/index.js
+++ src/_server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const db = require('./config')
 const cors = require('cors')
-
 const app = express();
 
 const PORT = 3002;
 app.use(cors());
 app.use(express.json())
+
+// IMPORTANT: To start up the middleware server, open a PowerShell window (or similar) in the /_server folder
+// and do the following command: node index.js
+// An output message saying "Server is running on 3002" should appear.
+
+// Below are two example queries. When the middleware server is running, if it receives a GET HTTP request 
+// (for example, from QuizPage.js via axios), then accept the request and run the query.
+// When the query completes, respond to the GET request by sending back the query result.
 
 // Route to get all questions by category
 app.get("/api/getCategoryQuestions", (req,res)=>{
@@ -28,6 +35,7 @@ app.get("/api/getCategoryLength", (req,res)=>{
         );   
 });
 
+// Listen for GET/POST requests on port 3002
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
 })
