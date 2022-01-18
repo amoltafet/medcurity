e2ee8853124ae106ac02ea6035590d2039c1febb
Madison Call <tcall@zagmail.gonzaga.edu>
2022-01-17 17:26:18 -0800

    Decided not to do that

--- src/LearningModule/LearningModule.js
+++ src/LearningModule/LearningModule.js
@@ -7,42 +7,6 @@ import { useEffect } from "react";
 import { useParams } from "react-router";
 
 /**
- * 
- * @param {str} category Question category to get questions from
- * @param {int} index Index of question in the returned query
- * @param {func} changeState Function to pass into question object to monitor its state
- */
- function getLearningModule(index)
- {
-   // The route for axios.get() to use to query the db
-   const url = 'http://localhost:3002/api//api/getLearningModule'
- 
-   // I am unsure why useState() and useEffect() makes this work,
-   // but plenty of online examples I viewed used this pattern.
-   const [question, getQuestions] = useState([]);
- 
-   useEffect(() => {
-     getQuestionsIncategory();
-   }, [])
- 
-   // Create a GET HTTP request that uses the getCategoryQuestions route to 
-   // query the db for questions in the specified category.
-   // Then choose a question from the query result using the index param.
-   const getQuestionsIncategory = () => {
-     axios.get(`${url}`, { params: { filter: category } }).then((response) => {
-       const data = response.data
-       getQuestions(data[index]);
-     }).catch(error => console.error(error));
-   }
- 
-   // Return the question component with question info from the query
-   // into the components props.
-   return (
-     {}
-   )
- }
-
-/**
 * Creates and displays the learning page for each test category. 
 * @return { LearningModule}
 */
