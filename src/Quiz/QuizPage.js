e9e18eaffb82461ac4cfe06e8b2165e6b73056a0
Aaron Miller <amiller26@zagmail.gonzaga.edu>
2022-01-24 00:49:45 -0800

    set up dynamic questions

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -14,50 +14,78 @@ import { useParams } from "react-router";
  * @param {int} index Index of question in the returned query
  * @param {func} changeState Function to pass into question object to monitor its state
  */
-function GetQuestionByIndex(category, index, changeState)
-{
-  // The route for axios.get() to use to query the db
-  const url = 'http://localhost:3002/api/getCategoryQuestions'
+// function GetQuestionByIndex(slug, index, changeState)
+// {
+//   // The route for axios.get() to use to query the db
+//   const url = 'http://localhost:3002/api/getCategoryQuestions'
 
-  // I am unsure why useState() and useEffect() makes this work,
-  // but plenty of online examples I viewed used this pattern.
-  const [question, getQuestions] = useState([]);
+//   // I am unsure why useState() and useEffect() makes this work,
+//   // but plenty of online examples I viewed used this pattern.
+//   const [question, getQuestions] = useState([]);
 
-  useEffect(() => {
-    getQuestionsIncategory();
-  }, [])
+//   useEffect(() => {
+//     getQuestionsIncategory();
+//   }, [])
 
-  // Create a GET HTTP request that uses the getCategoryQuestions route to 
-  // query the db for questions in the specified category.
-  // Then choose a question from the query result using the index param.
-  const getQuestionsIncategory = () => {
-    axios.get(`${url}`, { params: { filter: category } }).then((response) => {
-      const data = response.data
-      getQuestions(data[index]);
-    }).catch(error => console.error(error));
-  }
+//   // Create a GET HTTP request that uses the getCategoryQuestions route to 
+//   // query the db for questions in the specified category.
+//   // Then choose a question from the query result using the index param.
+//   useEffect(() => {
+//     axios.get('http://localhost:3002/api/getModuleQuestions', { params: { id: slug } }).then((response) => {
+//           setContent(Object.values(response.data))
+//       });
+//   }, [])
 
-  // Return the question component with question info from the query
-  // into the components props.
-  return (
-    <Questions
-    i = {index} 
-    question={question.question}
-    answers={[question.solution, question.a2, question.a3, question.a4]}
-    action={changeState} 
-    />
-  )
-}
+//   // Return the question component with question info from the query
+//   // into the components props.
+//   return (
+//     <Questions
+//     i = {index} 
+//     question={question.question}
+//     answers={[question.solution, question.a2, question.a3, question.a4]}
+//     action={changeState} 
+//     />
+//   )
+// }
 
 const QuizPage = () => {
     console.log('displaying quiz page');
+    const [content, setContent] = useState([])
 
     let { slug } = useParams();
     useEffect(() => {
       // Fetch post using the postSlug
     }, [slug]);
 
+    useEffect(() => {
+      axios.get('http://localhost:3002/api/getModuleQuestions', { params: { id: slug } }).then((response) => {
+            setContent(Object.values(response.data))
+        });
+    }, [])
+
     console.log('slug is ' + slug);
+
+    console.log(content);
+
+    var index = 0;
+
+    var quizTitle = ""
+
+    const QuestionContent = content.map((question) => {
+      index++;
+      quizTitle = question.category.charAt(0).toUpperCase() + question.category.slice(1);
+      return ([
+        <h3 className="question-title">
+          Question {index}
+        </h3>,
+        <Questions
+            i = {index} 
+            question={question.question}
+            answers={[question.solution, question.a2, question.a3, question.a4]}
+            action={adjustStateData} 
+          />
+      ]);
+    })
 
     // data array that holds question information using state
     const [data,setData]=useState([
@@ -86,13 +114,11 @@ const QuizPage = () => {
         <>
         <MenuBar></MenuBar>
         <Container className="quizPageContainer">
+          <h1>
+            {quizTitle} Quiz
+          </h1>
             <>
-              <h3> Question {'1'}: </h3> 
-              {GetQuestionByIndex('privacy', 0, adjustStateData)}
-              <h3> Question {'2'}: </h3> 
-              {GetQuestionByIndex('privacy', 1, adjustStateData)}
-              <h3> Question {'3'}: </h3> 
-              {GetQuestionByIndex('hipaa', 2, adjustStateData)}
+             {QuestionContent}
             </>
             <SubmitButton value="Submit" questionData={data}></SubmitButton>
         </Container>
