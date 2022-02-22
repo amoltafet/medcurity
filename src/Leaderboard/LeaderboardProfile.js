1737f10a85dd2d08a01e4961fabbfe7232405562
[rhale2] <[rhale2@zagmail.gonzaga.edu]>
2022-02-21 17:48:35 -0800

    merging

--- src/Leaderboard/LeaderboardProfile.js
+++ src/Leaderboard/LeaderboardProfile.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Accordion from 'react-bootstrap/Accordion'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Row, Card, Image, Col } from 'react-bootstrap';
+import { useEffect, useState } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
+import axios from 'axios';
 import './LeaderboardProfile.css'
 
 
@@ -13,172 +15,122 @@ import './LeaderboardProfile.css'
 * @return {GetPage}
 */
 function LeaderboardProfile(props) {
-    if (props.name === "jsmith") {
-    return (
-        <>
-        <Card className={`uvs-left uvs-right ${props.className[0]}`} style={{ flexDirection: 'row' }}>
-            <Accordion className="displayLeaderboardInfo" defaultActiveKey="0">
-                <Accordion.Toggle eventKey="1" className="accordianToggel">
-                <Card className="cardHeaderAccordian" style={{ flexDirection: 'row' }}>
-                    <Col sm>
-                        <div className="leaderboardRank">{props.index}.</div>
-                    </Col>
-                    <Col>
-                        <Image className={props.className[1]} src="/user.png" alt="" roundedCircle />
-                    </Col> 
-                    <Col sm>
-                        <Card.Text className="userNameTitle">{props.name}</Card.Text>
-                    </Col>  
-                    <Col sm>
-                        <div className="scoreLabelLeaderboard" >Score</div>
-                    </Col>  
-                    <Col>
-                        <div className="userPointsLeaderboard">{props.user.overallPoints}</div>
-                    </Col>
-                    
-                </Card>
-            
-                </Accordion.Toggle>
-                <Accordion.Collapse className="accordianCollapse" eventKey="1">
-     
-                       <Row>
-                        <Col>
-                        <Card.Text className={props.className[2]}>Category 1</Card.Text>
-                        <CircularProgressbar
-                            className={props.className[3]}
-                            value={props.user.category1TotalPoints}
-                            text={`${props.user.category1TotalPoints}%`}
-                            styles={{
-                                path: {
-                                    stroke: "#1e5b88",
-                                },
-                                trail: {
-                                    stroke: "#d8dae3",
-                                },
-                                text: {
-                                    fill: "white",
-                                    textAnchor: "middle",
-                                }
-                            }} />
-                    </Col>
-                    <Col>
-                        <Card.Text className={props.className[2]}>Category 2</Card.Text>
-                        <CircularProgressbar
-                            className={props.className[3]}
-                            value={props.user.category2TotalPoints}
-                            text={`${props.user.category2TotalPoints}%`}
-                            styles={{
-                                path: {
-                                    stroke: "#1e5b88",
-                                },
-                                trail: {
-                                    stroke: "#d8dae3",
-                                },
-                                text: {
-                                    fill: "white",
-                                    textAnchor: "middle",
-                                }
-                            }} />
-                    </Col>
-                    <Col>
+    const [directories, setDirectories] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-                            <Card.Text className={props.className[2]}>Category 3</Card.Text>
-             
-                        <CircularProgressbar
-                            className={props.className[3]}
-                            value={props.user.category3TotalPoints}
-                            text={`${props.user.category3TotalPoints}%`}
-                            styles={{
-                                path: {
-                                    stroke: "#1e5b88",
-                                },
-                                trail: {
-                                    stroke: "#d8dae3",
-                                },
-                                text: {
-                                    fill: "white",
-                                    textAnchor: "middle",
-                                }
-                            }} />
+    // Query for getting LearningDirectories Directory info
+    useEffect(() => {
+        axios.get('http://localhost:3002/api/getQuery', { params: { the_query: "SELECT * FROM LearningModulesDirectory" } }).then((response) => {
+            setDirectories(Object.values(response.data))
+            setLoading(false);
+        });
+    }, [])
+
+    const GetCurrentModule = () => {
+        var categoryData = []
+        if (directories != null) {
+        for (var i = 0; i < directories.length; i++) {
+            console.log(directories[0])
+            categoryData.push( <>
+                <Row>
+                    <Col >
+                    <Card.Text className={props.className[2]}>{directories[i].Title}</Card.Text>
+                    <CircularProgressbar
+                        className={props.className[3]}
+                        value={props.user.category1TotalPoints}
+                        text={`${props.user.category1TotalPoints}%`}
+                        styles={{
+                            path: {
+                                stroke: "#1e5b88",
+                            },
+                            trail: {
+                                stroke: "#d8dae3",
+                            },
+                            text: {
+                                fill: "white",
+                                textAnchor: "middle",
+                            }
+                        }} />
+                        </Col>
+                        <Col>
+                    <Card.Text className={props.className[2]}>Score</Card.Text>
                     </Col>
-                    <Col>
-                   
-                            <Card.Text className={props.className[2]}>Category 4</Card.Text>
-                    
-                        <CircularProgressbar
-                            className={props.className[3]}
-                            value={props.user.category4TotalPoints}
-                            text={`${props.user.category4TotalPoints}%`}
-                            styles={{
-                                path: {
-                                    stroke: "#1e5b88",
-                                },
-                                trail: {
-                                    stroke: "#d8dae3",
-                                },
-                                text: {
-                                    fill: "white",
-                                    textAnchor: "middle",
-                                }
-                            }} />
-                    </Col>
-                    <Col>
-                       
-                            <Card.Text className={props.className[2]}>Category 5</Card.Text>
-                      
-                        <CircularProgressbar
-                            className={props.className[3]}
-                            value={props.user.category5TotalPoints}
-                            text={`${props.user.category5TotalPoints}%`}
-                            styles={{
-                                path: {
-                                    stroke: "#1e5b88",
-                                },
-                                trail: {
-                                    stroke: "#d8dae3",
-                                },
-                                text: {
-                                    fill: "white",
-                                    textAnchor: "middle",
-                                }
-                            }} />
-                    </Col>
-                    </Row>
-                 
-                </Accordion.Collapse>
-            </Accordion>
-             </Card>
-        </>
-    );
+                </Row> 
+               
+            </>)
+           
+        } 
+        return (categoryData)
+        }
+    }
+
+
+    if (props.name === "jsmith") {
+        return (
+            <>
+                <Card className={`uvs-left uvs-right ${props.className[0]}`} style={{ flexDirection: 'row' }}>
+                    <Accordion className="displayLeaderboardInfo" defaultActiveKey="0">
+                        <Accordion.Toggle eventKey="1" className="accordianToggel">
+                            <Card className="cardHeaderAccordian" style={{ flexDirection: 'row' }}>
+                                <Col sm>
+                                    <div className="leaderboardRank">{props.index}.</div>
+                                </Col>
+                                <Col>
+                                    <Image className={props.className[1]} src="/user.png" alt="" roundedCircle />
+                                </Col>
+                                <Col sm>
+                                    <Card.Text className="userNameTitle">{props.name}</Card.Text>
+                                </Col>
+                                <Col sm>
+                                    <div className="scoreLabelLeaderboard" >Score</div>
+                                </Col>
+                                <Col>
+                                    <div className="userPointsLeaderboard">{props.user.overallPoints}</div>
+                                </Col>
+
+                            </Card>
+
+                        </Accordion.Toggle>
+                        <Accordion.Collapse className="accordianCollapse" eventKey="1">
+
+                            <Row className="categorgiesLeaderboardRow">
+                                {GetCurrentModule()}
+                            </Row>
+
+                        </Accordion.Collapse>
+                    </Accordion>
+                </Card>
+            </>
+        );
     }
     else {
         return (
             <>
-            <Card className={`uvs-left uvs-right ${props.className[0]}`} style={{ flexDirection: 'row' }}>
-                <Accordion className="displayLeaderboardInfo" defaultActiveKey="0">
-                    <Accordion.Toggle eventKey="1" className="accordianToggel">
-                    <Card className="cardHeaderAccordian" style={{ flexDirection: 'row' }}>
-                        <Col sm>
-                            <div className="leaderboardRank">{props.index}.</div>
-                        </Col>
-                        <Col>
-                            <Image className={props.className[1]} src="/user.png" alt="" roundedCircle />
-                        </Col> 
-                        <Col sm>
-                            <Card.Text className="userNameTitle">{props.name}</Card.Text>
-                        </Col>  
-                        <Col sm>
-                            <div className="scoreLabelLeaderboard" >Score</div>
-                        </Col>  
-                        <Col>
-                            <div className="userPointsLeaderboard">{props.user.overallPoints}</div>
-                        </Col>
-                        
-                    </Card>
-                
-                    </Accordion.Toggle>
-                </Accordion>
-                 </Card>
+                <Card className={`uvs-left uvs-right ${props.className[0]}`} style={{ flexDirection: 'row' }}>
+                    <Accordion className="displayLeaderboardInfo" defaultActiveKey="0">
+                        <Accordion.Toggle eventKey="1" className="accordianToggel">
+                            <Card className="cardHeaderAccordian" style={{ flexDirection: 'row' }}>
+                                <Col sm>
+                                    <div className="leaderboardRank">{props.index}.</div>
+                                </Col>
+                                <Col>
+                                    <Image className={props.className[1]} src="/user.png" alt="" roundedCircle />
+                                </Col>
+                                <Col sm>
+                                    <Card.Text className="userNameTitle">{props.name}</Card.Text>
+                                </Col>
+                                <Col sm>
+                                    <div className="scoreLabelLeaderboard" >Score</div>
+                                </Col>
+                                <Col>
+                                    <div className="userPointsLeaderboard">{props.user.overallPoints}</div>
+                                </Col>
+
+                            </Card>
+
+                        </Accordion.Toggle>
+                    </Accordion>
+                </Card>
             </>
         );
     }
