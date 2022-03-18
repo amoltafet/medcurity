ccb8b39fa64f3686451fb6692adba9f98d74ebfc
Ethan Ching <evpching@gmail.com>
2022-03-17 19:54:45 -0700

    fix origin port for prod/dev

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const logger = require('./logger').log;
 const express = require('express');
-const cors = require('cors')
-const serverConfig = require('./serverConfig.json')
+const cors = require('cors');
+const path = require('path');
+const serverConfig = require('./serverConfig.json');
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -13,15 +14,16 @@ const session = require("express-session");
 */
 
 const LISTEN_PORT = serverConfig.server.LISTEN_PORT
-const ORIGIN_PORT = serverConfig.server.DEV_PORT
+const ORIGIN_PORT = serverConfig.server.ORIGIN_PORT
 const BASE_URL = serverConfig.server.BASE_URL
+const METHODS = serverConfig.server.METHODS
 const SECRET = serverConfig.server.SECRET
 
 app.use(
   express.json(),
   cookieParser(),
   bodyParser.urlencoded({ extended: true }),
-  cors({ origin: [`${BASE_URL + ORIGIN_PORT}`], methods: ["GET", "POST"], credentials: true }),
+  cors({ origin: [`${BASE_URL + ORIGIN_PORT}`], methods: METHODS, credentials: true }),
   session({ resave: true, saveUninitialized: true, secret: SECRET })
 );
 
@@ -33,15 +35,15 @@ app.use('/users', usersRouter);
 app.use('/api', queryRouter);
 //app.use('/admin', adminRouter)
 
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/client/build')));
+// Serve static files from the React app, must be uncommented for production
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.listen(LISTEN_PORT, (err) => {
     
     if (err) console.log('ERROR: ', err)
 
     console.log(`API is running on ${LISTEN_PORT}`)
-    console.log(`Server is running on ${ORIGIN_PORT}`)
+    console.log(`Server is running on ${BASE_URL + ORIGIN_PORT}`)
     logger.log('info', `Server is running on "PORT: ${ORIGIN_PORT}"`, { service: 'api-service' })
 
 })
