11653604c3dc3fec4d7c162dd8350971027f4b05
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2022-10-31 20:44:39 -0700

    added nav

--- src/MenuBar/MenuBar.js
+++ src/MenuBar/MenuBar.js
@@ -281,7 +281,7 @@ const Menubar = () => {
 
       <form class="d-flex input-group w-auto ms-lg-3 my-3 my-lg-0">
         <input type="search" class="form-control" placeholder="Search Modules" aria-label="Search" />
-        <button class="btn btn-primary" type="button" data-mdb-ripple-color="dark">
+        <button class="btn search-btn-color" type="button" data-mdb-ripple-color="dark">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
         </svg>
