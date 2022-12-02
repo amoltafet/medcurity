7c11d6b00dde42f96251d94e114885b6e326de23
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2022-12-01 17:14:44 -0800

    done

--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -602,10 +602,10 @@ const QuizPage = () => {
         <MenuBar></MenuBar>
           <div id="quizPageContainer" className="quizBg img-fluid text-center justify-content-center">
               <div className="row">
-              <div className='col-md-auto'>
-                <LeftQuizBar></LeftQuizBar>
+              <div className='col mt-5'>
+                <LeftQuizBar data={content} />
               </div>
-              <div className='col'>
+              <div className='col-lg-9'>
                 <div className="questionPosOutOfTotal text-center" id="questionPosOutOfTotal"> {index + 1} / {content.length} </div>
                 {DisplayOneQuestion()}
                 <Button
@@ -713,18 +713,41 @@ const QuizPage = () => {
   }
 }
 
-const LeftQuizBar = () => {
+const LeftQuizBar = (data) => {
+
+  const getQuestionType = (type) => {
+    if (type === 'mc') {
+      return 'Multiple Choice'
+    } else if (type === 'fill') {
+      return 'Fill in the Blank'
+    } else if (type === 'match') {
+      return 'Matching'
+    } else {
+      return 'None'
+    }
+  }
+
   return (
     <div className="container mt-5">
-      <div className="card px-5 py-3">
-          <h3>Quiz Questions</h3>
+      <div className="card px-3 py-3">
+          <h5>Quiz Questions</h5>
         <div className="card-body">
-          <div className="list-group">
-            <a href="#q-group0" className="list-group-item list-group-item-action">Question 1</a>
-            <a href="#q-group1" className="list-group-item list-group-item-action">Question 2</a>
-            <a href="#q-group2" className="list-group-item list-group-item-action">Question 3</a>
-            <a href="#q-group3" className="list-group-item list-group-item-action">Question 4</a>
-            <a href="#q-group4" className="list-group-item list-group-item-action">Question 5</a>
+          <div className="list-group list-group text-start">
+             {data.data.map((question, index) => {
+              return (
+                <>
+                <p className="list-group-item list-group-item-action"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle-fill me-5" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10.354 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                    <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911l-1.318.016z"/>
+                  </svg>Question {index+1}. {getQuestionType(question.type)}
+               
+                
+              
+                </p>
+                
+              </>
+              );
+            })}
           </div>
         </div>
       </div>
