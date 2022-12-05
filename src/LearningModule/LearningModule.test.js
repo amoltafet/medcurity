943614d7eb26e8c35328048cb3569da54557df55
lmartin5 <lmartin5@zagmail.gonzaga.edu>
2022-12-04 21:07:02 -0800

    all tests working now

--- src/LearningModule/LearningModule.test.js
+++ src/LearningModule/LearningModule.test.js
@@ -2,7 +2,7 @@ import renderer from 'react-test-renderer';
 import LearningModule from './LearningModule';
 import React from 'react';
 
-test('renders LoginPage', () => {
+test('renders LearningModule', () => {
   const tree = renderer.create(<LearningModule />).toJSON();
   expect(tree).toMatchSnapshot();
 });
