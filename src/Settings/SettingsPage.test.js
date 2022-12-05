fda546328819fdf3b30ea1461916228bb7f3c37c
lmartin5 <lmartin5@zagmail.gonzaga.edu>
2022-12-04 20:41:56 -0800

    register page and settings tests fixed

--- src/Settings/SettingsPage.test.js
+++ src/Settings/SettingsPage.test.js
@@ -1,8 +1,9 @@
 import renderer from 'react-test-renderer';
 import SettingsPage from './SettingsPage';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders SettingsPage', () => {
-  const tree = renderer.create(<SettingsPage />).toJSON();
+  const tree = renderer.create(<BrowserRouter><SettingsPage /></BrowserRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
