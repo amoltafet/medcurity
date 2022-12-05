f523c173b84e4f942aa4d5ec99cd8fac59b2375e
lmartin5 <lmartin5@zagmail.gonzaga.edu>
2022-12-04 21:00:45 -0800

    Login page and menu bar tests fixed

--- src/Login/LoginPage.test.js
+++ src/Login/LoginPage.test.js
@@ -1,8 +1,9 @@
 import renderer from 'react-test-renderer';
 import LoginPage from './LoginPage';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders LoginPage', () => {
-  const tree = renderer.create(<LoginPage />).toJSON();
+  const tree = renderer.create(<BrowserRouter><LoginPage /></BrowserRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/MenuBar/Menubar.test.js
+++ src/MenuBar/Menubar.test.js
@@ -1,8 +1,9 @@
 import renderer from 'react-test-renderer';
 import Menubar from './MenuBar';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders LoginPage', () => {
-  const tree = renderer.create(<Menubar />).toJSON();
+test('renders Menubar', () => {
+  const tree = renderer.create(<BrowserRouter><Menubar /></BrowserRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
