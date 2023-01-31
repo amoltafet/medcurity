1f75d6bc082fb9892c171b2a302600f449c371ca
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2023-01-31 14:51:49 -0800

    base

--- src/MenuBar/MenuBar.js
+++ src/MenuBar/MenuBar.js
@@ -5,6 +5,7 @@ import { Nav, CardImg, Card, Row, Col, OverlayTrigger, Tooltip, Dropdown, Navbar
 import { useEffect, useState } from "react";
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import Notifications from './Notifications/Notifications'
 // import env from "react-dotenv";
 
 class SearchResults extends React.Component {
@@ -279,13 +280,24 @@ const Menubar = () => {
       <ul class="navbar-nav ms-auto d-flex flex-row mt-3 mt-lg-0">
       <div class="vertical-line"/>
         <li class="nav-item text-center mx-2 mx-lg-1">
-          <a class="nav-link" href="/notifications">
-            <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
-            </svg>
-            </div>
-          </a>
+         
+        <Navbar.Toggle aria-controls="navbar-dark-example" />
+        <Navbar.Collapse id="navbar-dark-example">
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title={ <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+          </svg>}
+              menuVariant="dark"
+              size="lg"
+              bsStyle="dark"
+              noCaret={true}
+            >
+              <Notifications/>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
         </li>
         <div class="vertical-line"/>
         {currentUser.type === "websiteAdmin"? 
---
+++ src/MenuBar/Notifications/NotifData.js
@@ -0,0 +1,25 @@
+const NotifData = [
+    {
+        id: 1,
+        title: 'New User',
+        time: 'Just now',
+        icon: 'user-plus',
+        color: 'primary'
+    },
+    {
+        id: 2,
+        title: 'Module Added',
+        time: '2 min ago',
+        icon: 'shopping-cart',
+        color: 'success'
+    },
+    {
+        id: 3,
+        title: 'Badge Earned',
+        time: '5 min ago',
+        icon: 'server',
+        color: 'danger'
+    },
+]
+
+export default NotifData;
