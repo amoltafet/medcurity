7c1edf96b4ba3e570356bbf19293fd96d8d89ba3
Ahmad Moltafet <ahmad.moltafet@IntelliTect.com>
2023-02-24 02:28:07 +0000

    Updated app.js

--- server/app.js
+++ server/app.js
@@ -81,7 +81,7 @@ else
   console.log('-- Launched with node --')
   const LISTEN_PORT = process.env.LISTEN_PORT || 3002
 
-  app.listen(LISTEN_PORT, '0.0.0.0', (err) => {
+  app.listen(LISTEN_PORT, '40.112.243.103', (err) => {
     console.log('API CONNECTION INFO:')
     console.log('\t- HTTP API Server running on PORT: ' + LISTEN_PORT);
     console.log(`\t- API is accessible at is: http://localhost:${LISTEN_PORT}`)
