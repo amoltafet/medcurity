be6a967ddf9fca7ac399a58ff2441675c2aae49d
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2023-03-02 14:48:52 -0800

    g

--- server/app.js
+++ server/app.js
@@ -9,14 +9,14 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
 const app = express();
-const cookieSettings = (process.env.COOKIES === 'enabled') ? { httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 48, sameSite: 'none' } : null;
+//const cookieSettings = (process.env.COOKIES === 'enabled') ? { httpOnly: true, secure: true, maxAge: 1000 * 60 * 60 * 48, sameSite: 'none' } : null;
 
 app.use(
   express.json(),
   cookieParser(),
   bodyParser.urlencoded({ extended: true }),
   cors({ origin: true, methods: ["GET", "POST"], credentials: true }),
-  session({ resave: false, saveUninitialized: true, secret: process.env.SECRET, cookie: cookieSettings })
+  
 );
 
 var usersRouter = require('./routes/usersRoutes');
@@ -42,7 +42,6 @@ app.listen(LISTEN_PORT, (err) => {
     console.log('API CONNECTION INFO:')
     console.log('\t- HTTP API Server running on PORT: ' + LISTEN_PORT);
     console.log(`\t- API is accessible at is: http://localhost:${LISTEN_PORT}`);
-    console.log(cookieSettings ? '\t- Cookies are enabled' : '\t- Cookies are disabled.')
     console.log(err ? err : '')
   }).on('error', (err) => {
     console.log(err);
