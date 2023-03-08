92e86518dfed04e5146a80b1ca20c4816e7d34c9
Elicke <mkylee2@gmail.com>
2023-03-07 16:09:08 -0800

    routes, controllers

---
+++ server/controllers/emailController.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+const transporterConfig = require('../nodemailerTransporterConfig.json');
+
+const sendEmployeeInvitationEmail = (req, res) => {
+    const transporter = nodemailer.createTransport(transporterConfig);
+
+    const mailOptions = {
+        from: "medtestsender",
+        to: req.body.email,
+        subject: "Success?",
+        text: "Success!"
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+};
+
+module.exports =
+{
+    sendEmployeeInvitationEmail
+};
